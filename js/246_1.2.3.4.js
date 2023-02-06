e84413b5ff3036f47782afca8ff4cebc17f16ca4
Dazaikinn <122983338+Dazaikinn@users.noreply.github.com>
2023-02-06 19:08:31 +0500

    Add files via upload

---
+++ js/246_1.2.3.4.js
@@ -0,0 +1,19 @@
+// let button1.addEventListener('dblclick', function() {
+// 	console.log('Hello World!');
+// });
+
+//let button2.addEventListener('mouseover', function() {
+// 	console.log('1');
+// });
+
+// let button3.addEventListener('mouseout', function() {
+// 	console.log('2');
+// });
+
+// let button4.addEventListener('mouseover', function() {
+// 	console.log('Привет');
+// });
+
+// let button4.addEventListener('mouseout', function() {
+// 	console.log('Пока');
+// });
